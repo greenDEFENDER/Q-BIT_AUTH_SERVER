e40195adc6a3704d799511c3331b14a4b3ab8483
greenDEFENDER <udaysah098@gmail.com>
2022-07-17 10:38:43 +0530

    some refactoring

--- src/main/java/bit/quantum/controller/AuthorizationController.java
+++ src/main/java/bit/quantum/controller/AuthorizationController.java
@@ -37,31 +37,7 @@ public class AuthorizationController {
         String password = request.getHeader("password");
 
         return AuthenticationUtil.authenticate(username, password, response);
-
-//        //validating that there are value in credentials
-//        if (!isCredentialAcceptabel(username, password))
-//            return new ResponseEntity<>("invalid credentials", HttpStatus.BAD_REQUEST);
-//
-//        // getting user from database.
-//        MyUser user = repo.findUser(username);
-//
-//        // validating user credentials.
-//        if (!passwordEncoder.matches(password, user.getPassword()))
-//            return new ResponseEntity<>("Invalid credentials", HttpStatus.BAD_REQUEST);
-//
-//        // generate secret.
-//        String secret = user.generateSecret();
-//        repo.updateSecret(username, secret);
-//
-//        // Building a json web token.
-//        String jwt = buildToken(user, secret);
-//
-//        // adding token to response header.
-//        response.setHeader("Authorization", jwt);
-//
-//        return new ResponseEntity<>("authenticated", HttpStatus.ACCEPTED);
     }
-
 
     // find secret of user.
     @GetMapping("/secret/{username}")
@@ -73,61 +49,5 @@ public class AuthorizationController {
     @GetMapping("/revokeToken")
     public String revokeToken(HttpServletRequest request) {
         return AuthenticationUtil.revokeToken(request.getHeader("Authorization"));
-
-        //  String jwt = request.getHeader("Authorization");
-        // String username = getUserNameFromJwt(jwt);
-        // String secret = repo.getSecret(username);
-        // if (!validateToken(jwt, secret))
-//            return "token already invalid";
-//        repo.updateSecret("invalid", username);
-//        return "token invalidated";
     }
-
-//    private boolean validateToken(String jwt, String secret) {
-//        try {
-//            SecretKey key = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
-//            Claims claims = Jwts.parserBuilder()
-//                    .setSigningKey(key)
-//                    .build()
-//                    .parseClaimsJws(jwt)
-//                    .getBody();
-//        } catch (Exception e) {
-//            return false;
-//        }
-//        return true;
-//    }
-//
-
-//    //get username from jwt token.
-//    private String getUserNameFromJwt(String jwt) {
-//        int i = jwt.lastIndexOf('.');
-//        String jwtWithoutSignature = jwt.substring(0, i + 1);
-//
-//        // @formatter:off
-//        Claims untrusted = Jwts.parserBuilder()
-//                .build()
-//                .parseClaimsJwt(jwtWithoutSignature)
-//                .getBody();
-//        // @formatter:on
-//
-//        return untrusted.get("username").toString();
-//    }
-
-//    private boolean isCredentialAcceptabel(String username, String password) {
-//        return (username != null && !username.isEmpty()) && (password != null && !password.isEmpty());
-//    }
-
-//    private String buildToken(MyUser user, String secret) {
-//        //creating signing key based on user secret.
-//        SecretKey signingKey = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
-
-//        return Jwts.builder()
-//                .addClaims(Map.of("username", user.getUsername()))
-//                .addClaims(Map.of("authorities", user.getRoles()))
-//                .setIssuedAt(Date.from(Instant.now()))
-//                //.setExpiration()
-//                .signWith(signingKey)
-//                .compact();
-//    }
-
 }
